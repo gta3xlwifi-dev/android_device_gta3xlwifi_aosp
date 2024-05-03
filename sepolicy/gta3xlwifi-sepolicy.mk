5e65d469ffe96ba48bd034bd6072b9da3d038a5a
ramenrolls <m.crnicki99@gmail.com>
2024-05-03 19:21:46 +0200

    - Specify the board information file. - Set the bootloader board name. - Enable parallel loading of kernel modules. - Specify that the exynos_drm kernel module should be loaded sequentially. - Include additional board-specific configuration files

---
+++ sepolicy/gta3xlwifi-sepolicy.mk
@@ -0,0 +1,5 @@
+# sepolicy shared among exynos devices
+BOARD_SEPOLICY_DIRS += device/samsung/gta3xlwifi
+
+# system_ext
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/samsung/gta3xlwifi/sepolicy/system_ext/
